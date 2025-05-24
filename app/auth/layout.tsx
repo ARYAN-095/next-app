a9f029a52f1e6c310d73743846b6a06dcd9bde6d
ARYAN-095 <aryanshukla095@gmail.com>
2025-05-24 07:01:33 +0000

    merge routes

---
+++ app/auth/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const layout = ({children}) :{
+    children: React.ReactNode
+}=> {
+  return (
+    <>
+    <div className='border-b text-center font-bold text-3xl'>Login now to get 30% off</div>
+    {children}
+    </>
+  )
+}
+
+export default layout   
